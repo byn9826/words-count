22b1fe598eb959c7c92d2c8bc6fe7a076f4e6dfc
byn9826 <byn9826@gmail.com>
2020-12-01 20:57:01 -0500

    compile before publish

---
+++ webpack.config.js
@@ -0,0 +1,24 @@
+const path = require('path');
+
+module.exports = {
+  entry: path.join(__dirname, './src/globalWordsCount.js'),
+  output: {
+    path: path.join(__dirname, './dist'),
+    filename: 'index.js',
+    library: 'words-count',
+    libraryTarget: 'umd',
+    publicPath: '/dist/',
+    umdNamedDefine: true,
+  },
+  mode: 'production',
+  module: {
+    rules: [{
+      test: /\.(js)$/,
+      use: 'babel-loader',
+      exclude: '/node_modules/',
+    }],
+  },
+  resolve: {
+    extensions: ['.js']
+  }
+};
